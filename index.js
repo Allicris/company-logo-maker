19084dfed3d83b325b53de087dec672093bfe51d
Allison Serrano <allisonserrano171@yahoo.com>
2023-06-28 20:46:59 -0400

    wrote the test file.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const inquirer = require("inquirer");
+const shapes = require('./lib/shapes');
+
+//Tutor ?'s - why is it that the user can input a color but in the shapes.test.js file a 'color' has to be set?
+// Can the input override the test? Is it just for testing purposes?
 //Array of questions for the user to input
 const questions = [
     {
